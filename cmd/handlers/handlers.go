43656d309134f34d20d294dca294714d89bb2461
zbessado <bessadouz@gmail.com>
2024-09-28 17:11:31 +0100

    something

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -20,8 +21,20 @@ func RegisterPage(w http.ResponseWriter, r *http.Request) {
 
 func LoginPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		http.Error(w , "method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	if r.Method == http.MethodPost {
+		fmt.Println("waaa adrari")
+	}
+	err := r.ParseForm() 
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	fmt.Println(name, email, password)
 }
