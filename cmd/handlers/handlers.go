935a01547d019a8a597569163a883fa936eac258
hamza <hamzasaliheddine@hotmail.com>
2024-09-28 13:16:30 +0100

    added handlers

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -1,1 +1,12 @@
 package handlers
+
+import "net/http"
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+}
+
+func RegisterPage(w http.ResponseWriter, r *http.Request) {
+}
+
+func LoginPage(w http.ResponseWriter, r *http.Request) {
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"net/http"
+
+	"forum/cmd/handlers"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.HomePage)
+	http.HandleFunc("/login", handlers.LoginPage)
+	http.HandleFunc("register", handlers.RegisterPage)
+	http.ListenAndServe(":8080", nil)
+}
