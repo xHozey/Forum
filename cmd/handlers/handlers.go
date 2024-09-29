18ceb971cd33b43a4e478b6bb92796fa46f8c925
hamza <hamzasaliheddine@hotmail.com>
2024-09-29 18:22:28 +0100

    added sessions and cookies but still probleme in cookies

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -3,50 +3,106 @@ package handlers
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/gofrs/uuid"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func HomePage(w http.ResponseWriter, r *http.Request) {
+func (d *MyDB) HomePage(w http.ResponseWriter, r *http.Request) {
 	tmp, err := template.ParseFiles("./cmd/templates/index.html")
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
 	}
+
+	c, err := r.Cookie("session_token")
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(w, r, "/login", http.StatusFound) // Redirect to login if cookie not found
+		return
+	}
+
+	qry, err := d.MyData.Query("SELECT uid FROM login WHERE uid = ?", c.Value)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	defer qry.Close()
+
+	if qry.Next() {
+		fmt.Println("youre good")
+	} else {
+		fmt.Println("session invalid")
+		http.Redirect(w, r, "/login", http.StatusFound)
+		return
+	}
+
 	tmp.Execute(w, nil)
 }
 
 func (d *MyDB) RegisterPage(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("/home/hamza/Desktop/Forum/cmd/templates/register.htm")
+	tmp, err := template.ParseFiles("./cmd/templates/register.htm")
 	if err != nil {
-		fmt.Println(err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	name := r.FormValue("name")
+	name := r.FormValue("user")
 	password := r.FormValue("password")
-	statement, err := d.MyData.Prepare("INSERT INTO login (user, pass) VALUES (?,?)")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+	if name != "" && password != "" {
+		qry, err := d.MyData.Query("SELECT user FROM login WHERE user = ?", name)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		defer qry.Close()
+
+		for qry.Next() {
+			var user string
+			err = qry.Scan(&user)
+			if err != nil {
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+				return
+			}
+			if user == name {
+				http.Error(w, "user already exists", http.StatusConflict)
+				return
+			}
+		}
+
+		statement, err := d.MyData.Prepare("INSERT INTO login (user, pass, uid) VALUES (?, ?, ?)")
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		defer statement.Close()
+
+		_, err = statement.Exec(name, password, "")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+		http.Redirect(w, r, "/login", http.StatusMovedPermanently)
+
 	}
-	statement.Exec(name, password)
+
 	tmp.Execute(w, nil)
 }
 
 func (d *MyDB) LoginPage(w http.ResponseWriter, r *http.Request) {
-	tmp, err := template.ParseFiles("./cmd/templates/index.html")
+	tmp, err := template.ParseFiles("./cmd/templates/login.html")
 	if err != nil {
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	name := r.FormValue("name")
+	name := r.FormValue("user")
 	password := r.FormValue("password")
-	statement, err := d.MyData.Query("SELECT (user, pass) FROM login")
+	statement, err := d.MyData.Query("SELECT user, pass FROM login")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
@@ -55,8 +111,23 @@ func (d *MyDB) LoginPage(w http.ResponseWriter, r *http.Request) {
 		var user, pass string
 		statement.Scan(&user, &pass)
 		if user == name && password == pass {
-			http.Redirect(w, r, "/", 202)
+			u := uuid.Must(uuid.NewV4())
+			uid := u.String()
+			stm, err := d.MyData.Prepare("UPDATE login SET uid = ? WHERE user = ?")
+			if err != nil {
+				log.Fatal(err)
+			}
+			stm.Exec(uid, user)
+			http.SetCookie(w, &http.Cookie{
+				Name:   "session_token",
+				Value:  uid,
+				Path:   "/",
+				MaxAge: 60,
+			})
+
+			http.Redirect(w, r, "/", http.StatusMovedPermanently)
 		}
 	}
+
 	tmp.Execute(w, nil)
 }
--- cmd/main.go
+++ cmd/main.go
@@ -18,14 +18,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	stm, err := db.Prepare("CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, pass TEXT)")
+	stm, err := db.Prepare("CREATE TABLE IF NOT EXISTS login (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT NOT NULL, pass TEXT NOT NULL, uid TEXT)")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
 	}
 	stm.Exec()
 	data.MyData = db
-	http.HandleFunc("/", handlers.HomePage)
+	http.HandleFunc("/", data.HomePage)
 	http.HandleFunc("/login", data.LoginPage)
 	http.HandleFunc("/register", data.RegisterPage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
