0372feb8be985be9e09d09a3edc1ebb1b383b395
Your Name <you@example.com>
2024-10-01 13:24:29 +0100

    create all struct

---
+++ cmd/model/Posts.go
@@ -0,0 +1,9 @@
+package model
+
+type Post struct {
+    ID      int    `json:"id"`
+    UserID  int    `json:"user_id"`
+    Title   string `json:"title"`
+    Content string `json:"content"`
+    Images  string `json:"images"`
+}
